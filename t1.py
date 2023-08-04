8379e3fa35a84bd306f39d8997a90f058c1e95d1
Bruno Gondim Toledo <toledis@gmail.com>
2023-08-04 13:01:18 -0300

    .

--- t1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn import feature_extraction, linear_model, model_selection, preprocessing
-
-train_df = pd.read_csv("train.csv")
-test_df = pd.read_csv("test.csv")
